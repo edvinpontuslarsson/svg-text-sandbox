13f0fdbb2659122e2262788f75b1e839253169c2
Edvin Larsson <edvinpontuslarsson@gmail.com>
2022-03-03 17:50:12 +0100

    sandbox in progress

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,26 @@
-const encodedData = window.btoa()
+// const encodedData = window.btoa()
 
-const svg = `base64:etc"${encodedData}"`
+// const svg = `base64:etc"${encodedData}"`
+
+// just a function that logs svg, don't need all here
+
+// TODO continue here, also split by space
+
+const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae ligula eget justo ullamcorper ultricies ac eu ligula. Nam commodo nibh dui, at pharetra justo ultricies ut. Donec ornare molestie congue.";
+
+const limit = 11;
+
+const lines = [];
+
+let counter = 0;
+
+let tempString = "";
+
+for (let i = 0; i < text.length; i++) {
+  //
+
+
+  counter++;
+}
+
+const mockSvg = `<sandbox></sandbox>`;
