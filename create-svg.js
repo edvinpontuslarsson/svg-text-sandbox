52a0120c76ef1e2adf21903d02e49c217742c07e
Edvin Larsson <edvinpontuslarsson@gmail.com>
2022-03-09 13:15:38 +0100

    Initial svg (#1)
    * svg manually
    
    * bg color
    
    * bg color 28 28 28
    
    * todos
    
    * OK, better now
    
    * rm comments
    
    * xml formatting
    
    * js notes
    
    * less stuff
    
    * stuff in txt
    
    * bg color
    
    * closer to top
    
    * 2 rows
    
    * style
    
    * positioning
    
    * line break
    
    * sandbox in progress
    
    * longer txt limit
    
    * fixed top
    
    * todo comment
    
    * back to simpler 1 line only
    
    * gh likes extra line end
    
    * svg js in progress
    
    * parametizes
    
    * renames function
    
    * removes sandbox js

---
+++ create-svg.js
@@ -0,0 +1,41 @@
+/**
+ * @typedef ISVGTextParams
+ * @property {string} text
+ * @property {number | undefined} width
+ * @property {number | undefined} height
+ * @property {number | undefined} fontSize
+ */
+
+/**
+ * @param {ISVGTextParams} params
+ */
+function getSvgText(params) {
+  const text = params.text || "";
+  const width = params.width || 200;
+  const height = params.height || 44;
+  const fontSize = params.fontSize || 22;
+
+  return `
+    <svg width="${width}px" height="${height}px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <defs>
+            <filter id="rounded-corners-2" primitiveUnits="objectBoundingBox">
+                <feImage preserveAspectRatio="none" width="110%" height="110%" x="-5%" y="0%"
+                    xlink:href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' x='0px' y='0px' viewBox='0 0 400 40' height='40' width='400'%3E%3Crect fill='rgb(28, 28, 28)' x='0' y='0' rx='10' ry='10' width='400' height='40'/%3E%3C/svg%3E" />
+                <feComposite operator="over" in="SourceGraphic" />
+            </filter>
+        </defs>
+        <text fill="white" filter="url(#rounded-corners-2)" x="20" y="4">${text}</text>
+
+        <style>
+            <![CDATA[
+            text {
+                dominant-baseline: hanging;
+                font: ${fontSize}px Verdana, Helvetica, Arial, sans-serif;
+            }
+            ]]>
+        </style>
+    </svg>
+`;
+}
+
+console.log(getSvgText({ text: "hej hej" }));
